d49cc7b9e2fd4a8ca48e75156309a67d43209a9f
Tyron keet <tyron.keet.01@gmail.com>
2020-06-13 15:17:22 +0200

    finder proj

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export class Navbar extends Component {
-    render() {
+const Navbar = ({icon, title}) => {
         return (
-            <nav className='navbar bg-primary'>
+            <nav className="navbar bg-primary">
                 <h1>
-                    <i className="fab fa-github" /> Navbar
+                    <i className={icon} /> { title}
                 </h1>
+                <ul>
+                    <li>
+                       <Link to='/'>Home</Link>  
+                    </li>
+                    <li>
+                       <Link to='/about'>About</Link>  
+                    </li>
+                </ul>
             </nav>
         );
-    }
-}
+};
 
-export default Navbar
+Navbar.defaultProps = {
+    title: 'Github Finder',
+    icon: "fab fa-github"
+};
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+
+} 
+
+export default Navbar;
